b2e855cfe7448118d1c5b0f4e7223ad116ad69e6
Fernando Iglesias <fernando.iglesiasg@gmail.com>
2013-07-03 15:32:17 +0200

    Top-level LMNN algorithm. Lower-level functions WIP.

---
+++ lmnn.m
@@ -0,0 +1,78 @@
+function M = lmnn(x,y,k)
+%
+% TODO DOC
+%
+
+%%% preamble
+
+% get input dimension and number of examples from data
+[d, n] = size(x);
+% trade-off between pull and push forces in the objective or loss function
+mu = 0.5;
+% number of iterations between exact computation of impostors
+correction = 15;
+% learning rate used in gradient descent
+stepsize = 1e-09;
+% maximum number of iterations
+maxiter = 1000;
+% objective
+obj = zeros(1,maxiter);
+
+% the number of feature vectors must be equal to the number of labels
+assert(n == length(y))
+
+%%% initializations
+
+% distance, identity matrix
+M = eye(d);
+% iteration counter counter
+iter = 0;
+% previous active set of impostors, empty
+Np = {};
+% compute target or genuine neighbours
+gen = getGenNN(x,y,k);
+% (sub-)gradient
+G = (1-mu)*sumOuterProducts(x, gen(1,:), gen(2,:));
+% stop criterion
+stop = false;
+
+%%% main loop
+
+while ~stop && t < maxiter
+    
+    % impostors computation
+    if mod(iter,correction) == 0
+        % compute exactly the set of impostors
+        Ncex = getImp('exact', x, M, gen);
+        Nc = Ncex;
+    else
+        % approximate the set of impostors, \hat{Nc}
+        Nc = getImp('approx', Ncex, M);
+    end
+    
+    % (sub-)gradient computation
+    G = updateGradient(G, mu, x, Nc, Np);
+    % take gradient step in the distance and project onto SDP cone
+    M = sdpProject(M - stepsize*G);
+    
+    % update iteration counter
+    iter = iter+1;
+    % compute objective
+    obj(iter) = lmnnObj(mu, x, gen, Nc);
+
+    % correct stepsize
+    if iter > 1
+        % difference between current and previous objective
+        delta = obj(iter) - obj(max(iter-1,1));
+        if delta > 0
+            % the objective has increased in this iteration
+            stepsize = stepsize*0.5;
+        else
+            % the objective has decreased
+            stepsize = stepsize*1.01;
+        end
+    end
+    
+    % update previous impostor set
+    Np = Nc;
+end
