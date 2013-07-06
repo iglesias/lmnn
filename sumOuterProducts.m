f51e94314f5fd79437c23aadcfa20a852186c152
Fernando Iglesias <fernando.iglesiasg@gmail.com>
2013-07-06 02:33:25 +0200

    Bug fix in SOP helper function. There was no variable n. Add also assert for safety

--- sumOuterProducts.m
+++ sumOuterProducts.m
@@ -3,7 +3,9 @@ function sop = sumOuterProducts(x,a,b)
 % TODO DOC
 %
 
+assert(length(a) == length(b))
 sop = zeros(size(x,1));
-for i = 1:n
+
+for i = 1:length(a)
   sop = sop + x(:, a(i))*x(:, b(i))';
 end;
