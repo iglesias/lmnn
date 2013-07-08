ba53c7d3cbec254093d54d2c4c800513f6cf604d
Fernando Iglesias <fernando.iglesiasg@gmail.com>
2013-07-08 04:25:17 +0200

    Bug fix in get impostors function. Index of the target neighbour was wrong in the impostors triplet

--- getImp.m
+++ getImp.m
@@ -7,6 +7,7 @@ N = [];
 
 if strcmp(mode, 'exact')
     
+    fprintf('>>>>> getImp: exact mode selected\n')
     % ensure that the correct number of arguments is given
     assert(length(varargin) == 2)
     % extract arguments
@@ -15,7 +16,7 @@ if strcmp(mode, 'exact')
     
     % get the number of data examples
     n = size(x,2);
-    % ensure that the gen second dimension makes sense
+    % ensure that gen's second dimension is a multiple of n
     assert(mod( size(gen,2), n ) == 0)
     % get the number of target neighbours used
     k = size(gen,2)/n;
@@ -27,21 +28,21 @@ if strcmp(mode, 'exact')
     % compute square distances to target neighbours plus margin
     Ni = zeros(k,n);
     for i = 1:k
-        Ni(i,:) = sum((Lx - Lx(:, gen(1, (i-1)*n+1:i*n))).^2, 1) +1; 
+        Ni(i,:) = sum( (Lx - Lx(:, gen(1, (i-1)*n+1:i*n) )).^2 ) +1;
     end
     
     %FIXME this is way too slow!!
     % compute impostors
     for i = 1:n % for each training example
         for j = 1:k % for each target neighbour
-            % get training examplex labelled differently
+            % get training examples labelled differently
             idxs = find(y~=y(i));
             for idx = idxs % for each possible impostor
                 % compute square distance to current training example
                 % and compare with the distance plus margin to the
                 % current target neighbour
-                if sum( (Lx(:,i)-Lx(:,idx)).^2, 1 ) <= Ni(j,i)
-                    N = [N [i;j;idx]];
+                if sum( (Lx(:,i)-Lx(:,idx)).^2 ) <= Ni(j,i)
+                    N = [N [i; gen(1, (j-1)*n+i); idx]];
                 end
             end
         end
@@ -50,11 +51,11 @@ if strcmp(mode, 'exact')
     
     
 elseif strcmp(mode, 'approx')
-    fprintf('approx mode selected\n')
+    fprintf('>>>>> getImp: approx mode selected\n')
     % ensure that the correct number of arguments is given
     assert(length(varargin) == 1)
     % extract arguments
-    Ncex = varargin{1};
+%     Ncex = varargin{1};
     
     %%%TODO
 else
