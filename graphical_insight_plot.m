a7360ca4ffde36c81ef36e2ff85ca051dc15d950
Fernando Iglesias <fernando.iglesiasg@gmail.com>
2013-08-22 09:14:53 +0200

    Add toy graphical demo

---
+++ graphical_insight_plot.m
@@ -0,0 +1,14 @@
+function graphical_insight_plot(x)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+plot(x(1,1:4),x(2,1:4),'oy','MarkerSize',10,'MarkerFaceColor','y')
+hold on
+plot(x(1,5:6),x(2,5:6),'sb','MarkerSize',10,'MarkerFaceColor','b')
+plot(x(1,7:8),x(2,7:8),'sr','MarkerSize',10,'MarkerFaceColor','r')
+hold off
+xlabel('x')
+ylabel('y')
+
+end
+
--- tests.m
+++ tests.m
@@ -106,3 +106,21 @@ disp('labels vector='), disp(y);
 
 L = lmnn(x,y,k);
 disp('L='), disp(L);
+
+%% graphical insight
+
+x=[0 0; -1 -0.1; 0.3 -0.05; 0.7 0.3; -0.2 -0.6; -0.15 -0.63; -0.25 0.55; -0.28 0.67]';
+y=[0 0 0 0 1 1 2 2];
+subplot(121)
+graphical_insight_plot(x)
+title('Original data')
+grid on
+
+L = lmnn(x,y,1);
+disp('L='), disp(L);
+
+Lx = L*x;
+subplot(122)
+graphical_insight_plot(Lx);
+title('Data transformed after LMNN')
+grid on
